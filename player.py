6ea552bb3946ec51e140fbd46bf1157c58daa603
harshachinna456 <63443758+harshachinna456@users.noreply.github.com>
2022-04-14 08:35:15 +0530

    first commit

---
+++ player.py
@@ -0,0 +1,83 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+        
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8):')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invali squatre. Try again.")
+
+        return val
+
+class GeniusComputerplayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+
+    def get_move(self, game):
+            if len(game.available_moves()) ==9:
+                square = random.choice(game.available_moves())
+            else:
+                square = self.minmax(game, self.letter)['position']
+            return square
+
+
+    def minmax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None,
+                    'score': 1*(state.num_empty_squares() +1) if other_player == max_player else -1*(state.num_empty_squares()+1)
+
+                    }
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+        
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            simulated_score = self.minmax(state, other_player)
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            simulated_score['position'] = possible_move
+
+            if player == max_player:
+                if simulated_score['score'] > best['score']:
+                    best = simulated_score
+            else:
+                 if simulated_score['score'] < best['score']:
+                    best = simulated_score
+        return best                    
+
